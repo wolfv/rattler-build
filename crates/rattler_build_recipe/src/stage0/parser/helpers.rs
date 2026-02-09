381654f671bcd0c3a382001d43051186c198cfe7
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-02-09 10:04:57 +0100

    remove unused imports

--- crates/rattler_build_recipe/src/stage0/parser/helpers.rs
+++ crates/rattler_build_recipe/src/stage0/parser/helpers.rs
@@ -1,11 +1,8 @@
 //! Helper functions for parsing
 
-use marked_yaml::{Node as MarkedNode, types::MarkedMappingNode};
+use marked_yaml::Node as MarkedNode;
 
-use crate::{
-    Span,
-    error::{ParseError, ParseResult},
-};
+use crate::Span;
 
 /// Get the span from a marked_yaml node
 pub(crate) fn get_span(node: &MarkedNode) -> Span {
